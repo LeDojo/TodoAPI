cd9e175fdedd68a5c9b30feb171fef7e31488eb3
J8T <jul.milcent@gmail.com>
2023-12-22 15:49:00 +0100

    :wrench: Database connected :wrench:

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/todo");
+  console.log("Database connected");
+}
+
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
